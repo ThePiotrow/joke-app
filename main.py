1d7296f3022f31f7b9e731e878e046b3c071f172
Maxime MARCHAND <mmarchand6@myges.fr>
2022-10-11 10:31:37 +0200

    Implement Jenkins

---
+++ main.py
@@ -0,0 +1,15 @@
+#Trouver les nombres premiers qui composent un nombre
+def factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+factors(4)
